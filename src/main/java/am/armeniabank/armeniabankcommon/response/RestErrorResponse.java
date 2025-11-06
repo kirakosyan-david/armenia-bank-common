a3050d0dfdf9480c643ddb6d8f0616764af7e22f
David <kirakosyandavit1995@gmail.com>
2025-11-06 16:38:31 +0400

    added common

---
+++ src/main/java/am/armeniabank/armeniabankcommon/response/RestErrorResponse.java
@@ -0,0 +1,19 @@
+package am.armeniabank.armeniabankcommon.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RestErrorResponse {
+
+    private int statusCode;
+
+    private String errorMessage;
+}
