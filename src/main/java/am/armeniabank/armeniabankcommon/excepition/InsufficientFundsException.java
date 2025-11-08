2627f50a7e980edb5a92e65cf2c7306ea23c6be3
David <kirakosyandavit1995@gmail.com>
2025-11-08 14:17:44 +0400

    added common

---
+++ src/main/java/am/armeniabank/armeniabankcommon/excepition/InsufficientFundsException.java
@@ -0,0 +1,27 @@
+package am.armeniabank.armeniabankcommon.excepition;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class InsufficientFundsException extends RuntimeException{
+
+    public InsufficientFundsException() {
+    }
+
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    public InsufficientFundsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InsufficientFundsException(Throwable cause) {
+        super(cause);
+    }
+
+    public InsufficientFundsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
